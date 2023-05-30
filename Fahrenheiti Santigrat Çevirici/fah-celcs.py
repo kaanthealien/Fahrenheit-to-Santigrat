f2a30833ddb3725083541caa858370d69923cd4f
Kaan <ilginkaan4022003@gmail.com>
2023-05-30 17:18:12 +0300

    Fahrenheiti Santigrat Çevirici
    Python ile Fahrenheiti Santigrat'a çevirme uygulaması.

---
+++ Fahrenheiti Santigrat Çevirici/fah-celcs.py
@@ -0,0 +1,4 @@
+derece=int(input("Lutfen bir derece giriniz: "))
+print("Girdiginiz derecenin Fahrenhet karsiligi: ",derece)
+celcius=(derece-32)*5/9
+print("Girdiginiz derecenin celciusa cevrilmis hali: ",celcius)
